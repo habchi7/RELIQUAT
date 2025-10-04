e12eb04a5300fb9ec2d282fe35fa723c55253304
habchi7 <69268077+habchi7@users.noreply.github.com>
2025-10-04 15:11:04 +0100

    Update RLQ.js

--- RLQ.js
+++ RLQ.js
@@ -179,7 +179,7 @@
         if (conditionMet && insertButton()) {
             clearInterval(interval); // Stop checking after button is added
         }
-    }, 500);
+    }, 200);
     // Button click event to run extraction and show popup
     button.addEventListener('click', async () => {
         // Create and display progress element
